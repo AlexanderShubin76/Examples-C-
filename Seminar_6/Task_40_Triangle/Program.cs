7150feb10f942f1c0adc0e2bf71bea717e2ca53b
Alexander <pastukh2007@yandex.ru>
2022-11-17 01:33:37 +0300

    Homework_6

---
+++ Seminar_6/Task_40_Triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа
+// и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+System.Console.Write("Введите число 1 (в диапазоне от 0 до 100): ");
+int number1;
+while (!int.TryParse(Console.ReadLine(), out number1) || number1<1 || number1>100)
+{
+    System.Console.WriteLine("Введите число 1 (в диапазоне от 1 до 100): ");
+}
+
+System.Console.Write("Введите число 2 (в диапазоне от 0 до 100): ");
+int number2;
+while (!int.TryParse(Console.ReadLine(), out number2) || number2<1 || number2>100)
+{
+    System.Console.WriteLine("Введите число 2 (в диапазоне от 1 до 100): ");
+}
+
+System.Console.Write("Введите число 3 (в диапазоне от 0 до 100): ");
+int number3;
+while (!int.TryParse(Console.ReadLine(), out number3) || number3<1 || number3>100)
+{
+    System.Console.WriteLine("Введите число 3 (в диапазоне от 1 до 100: ");
+}
+
+CompareSidesOfTriangle(number1, number2, number3);
+
+void CompareSidesOfTriangle(int n1, int n2, int n3)
+{
+    if (n1 + n2 >n3 && n2 + n3 >n1 && n1 + n3 >n2)
+        System.Console.WriteLine("Такой треугольник существует!");
+
+    else
+    {
+        System.Console.WriteLine("Такой треугольник не существует!");
+    }
+}
---
+++ Seminar_6/Task_42/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 42: Напишите программу, которая
+// будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+System.Console.WriteLine("Введите число (в диапазоне от 1 до 100): ");
+int number;
+while (!int.TryParse(Console.ReadLine(), out number) || number > 100 || number < 1)
+{
+    System.Console.WriteLine("Введите число в диапазоне от 1 до 100: ");
+}
+ConvertDigit(number);
+void ConvertDigit(int n)
+{
+    string emp = string.Empty;
+    for (int i = 0; n!=0; i++)
+    {
+        int k = n%2;
+        n /=2;
+        emp += k.ToString();
+    }
+    char[] arr = new char[emp.Length];
+    for (int j = 0; j < emp.Length; j++)
+    {
+        arr[j] = emp[^(j+1)];
+    }
+    System.Console.WriteLine(string.Join("", arr));
+}
